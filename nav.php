8c48ccdeffd1f0ac56a4f98c54ab254e5e6368ea
huangjia <huangjia86@yeah.net>
2014-11-09 16:49:49 +0800

    aa

---
+++ nav.php
@@ -0,0 +1,106 @@
+<script type="text/javascript">
+	var itemsArr = [
+      {
+        title:'日历',
+        href : 'calendar.php',
+        src : 'http://img03.taobaocdn.com/tps/i3/T1qjEbXidbXXc6QKnv-150-100.png',
+        desc:'<p>日历控件包括，日历选择器，日历控件，月份选择控件，时间选择控件。</p>'
+      },
+      {
+        title:'弹出框',
+        href : 'dialog.php',
+        src : '../assets/img/dialog.png',
+        desc:'<p>弹出框包括：模态窗口，非模态窗口，可以自定义内容和按钮，方便灵活的使用。</p>'
+      },
+      {
+        title:'消息提示',
+        href : 'message.php',
+        src : '../assets/img/message.png',
+        desc:'<p>提示/确认框包括：消息、警告、确认、错误已经成功提示，确认框同时包含确认和取消，都可以自定义内容和回调函数。</p>'
+      },
+      {
+        title:'菜单',
+        href : 'menu.php',
+        src : '../assets/img/menu.png',
+        desc:'<p>分为右键菜单，弹出菜单，左侧菜单以及各种自定义的菜单</p>'
+      },
+      {
+        title:'列表',
+        href : 'list.php',
+        src : '../assets/img/list.png',
+        desc:'<p>简单列表，复杂列表，灵活可配置的扩展列表</p>'
+      },
+      {
+        title:'选择框',
+        href : 'select.php',
+        src : '../assets/img/select.png',
+        desc:'<p>下拉列表，单选、多选、组合框、提示输入框等</p>'
+      },
+      {
+        title:'表单帮助类',
+        href : 'form-help.php',
+        src : '../assets/img/form-layout.jpg',
+        desc:'<p>获取表单生成的对象，把对象数据设置到表单上，给表单添加提示。</p>'
+      },
+      {
+        title:'表单',
+        href : 'form.php',
+        src : '../assets/img/form-page.jpg',
+        desc:'<p>集成了表单提示、表单验证、表单分组等功能，提供最简单的使用表单方式。</p>'
+      },
+      {
+        title:'标签（tab)',
+        href : 'tab.php',
+        src : '../assets/img/tab.png',
+        desc:'<p>标签（tab)，分为按钮式标签，导航式标签。</p>'
+      },
+      {
+        title:'简单表格',
+        href : 'simple-grid.php',
+        src : '../assets/img/simple-grid.png',
+        desc:'<p>简单表格：支持自定义列，自适应宽度表格，自定义单元格内容等常用功能，简单的显示数据。</p>'
+      },
+      {
+        title:'表格',
+        href : 'grid-local.php',
+        src : '../assets/img/local-grid.png',
+        desc:'<p>加载本地数据的表格，包含各种丰富功能的表格</p>'
+      },
+      {
+        title:'表格-分页',
+        href : 'grid-remote.php',
+        src : '../assets/img/page-grid.png',
+        desc:'<p>加载远程数据，分页</p>'
+      },
+      {
+        title:'表格-扩展',
+        href : 'grid-plugin.php',
+        src : '../assets/img/expand-grid.png',
+        desc:'<p>表格插件，单选、多选、行展开、列设置菜单、行分组、列分组</p>'
+      },
+      {
+        title:'表格-编辑',
+        href : 'edit-grid.php',
+        src : '../assets/img/edit-grid.png',
+        desc:'<p>单元格编辑、行编辑、使用Dialog编辑表格</p>'
+      },
+	    {
+        title:'工具栏',
+        href : 'bar.php',
+        src : '../assets/img/toolbar.jpg',
+        desc:'<p>存放各种操作的容器。</p>'
+      },
+      {
+        title:'进度条',
+        href : 'progress-bar.php',
+        src : '../assets/img/progressbar.png',
+        desc:'<p>展现加载进度。</p>'
+      },
+      {
+        title:'加载BUI',
+        href : 'loader.php',
+        src : '../assets/img/loading.gif',
+        desc:'<p>加载BUI的各种方式，seed、bui、kissy以及seajs</p>'
+      }
+    ];
+</script>
